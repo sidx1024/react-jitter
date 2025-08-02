14a54de8b271d0eb04e748595fb62ad2c691158a
sid <sid@apollo.io>
2025-08-01 20:02:52 -0700

    Delete transform

--- transform/tests_not_used_for_now/fixture/default/input.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function UserForm() {
-  const [count, setCount] = useState(0);
-  const nameFieldValues = useFieldValues("name");
-  const addressFieldValues = useFieldValues("address");
-
-  return <div />;
-}
